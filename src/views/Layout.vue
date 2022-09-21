9f6655a9ef638935c5d12a24dc30c82f8fc037a1
lihuahua <1213056130@qq.com>
2022-09-21 10:45:14 +0800

    完成首页主体-左侧分类-结构渲染

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -3,6 +3,8 @@
 <AppNavbar />
 <!--头部组件-->
 <AppHeader />
+<!--吸顶头部组件-->
+<AppHeaderSticky />
 <!--内容容器-->
 <div class="app-body">
   <!--二级路由-->
@@ -16,12 +18,14 @@
 import AppNavbar from '@/components/app-topnav'
 import AppHeader from '@/components/app-header'
 import AppFooter from '@/components/app-footer'
+import AppHeaderSticky from '@/components/app-header-sticky'
 import { useStore } from 'vuex'
 export default {
   components: {
     AppNavbar,
     AppHeader,
-    AppFooter
+    AppFooter,
+    AppHeaderSticky
   },
   name: 'LaYout',
   // 获取分类数据
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,19 +1,25 @@
 <template>
+<!--首页-->
   <div class="container">
-      HomePage
+      <!--首页入口-->
+      <div class="home-entry">
+        <!--左侧分类-->
+        <HomeCategory />
+        <!--轮播图-->
+      </div>
   </div>
 </template>
 
 <script>
+import HomeCategory from './components/home-category'
 export default {
+  components: {
+    HomeCategory
+  },
   name: 'HomePage'
 
 }
 </script>
 
 <style scoped lang="less">
-.container {
-  .hoverShadow();
-  color:@xtxColor
-}
 </style>
