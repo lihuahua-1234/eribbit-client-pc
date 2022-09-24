ab5f59a96eb3f0f22953565ed05de573cca9e3e6
lihuahua <1213056130@qq.com>
2022-09-24 10:19:08 +0800

    二级类目-复选框组件封装

--- src/views/category/sub.vue
+++ src/views/category/sub.vue
@@ -6,15 +6,22 @@
       <SubBread />
       <!--筛选区-->
       <SubFilter />
+      <!--测试复选框-->
+      <XtxCheckbox v-model="isAllCheckd">全选</XtxCheckbox>{{ isAllCheckd }}
     </div>
   </div>
 </template>
 <script>
+import { ref } from 'vue-demi'
 import SubBread from './components/sub-bread.vue'
 import SubFilter from './components/sub-filter.vue'
 export default {
   components: { SubBread, SubFilter },
-  name: 'SubCategory'
+  name: 'SubCategory',
+  setup () {
+    const isAllCheckd = ref(true)
+    return { isAllCheckd }
+  }
 }
 </script>
 <style scoped lang="less"></style>
